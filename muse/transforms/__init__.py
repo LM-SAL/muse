884923350ca24c6c2e82dcdefa97e75a0e004d81
Nabil Freij <nabil.freij@gmail.com>
2026-06-19 12:34:54 -0700

    feature: added reshape_slit_step_to_x

--- muse/transforms/__init__.py
+++ muse/transforms/__init__.py
@@ -1,3 +1,3 @@
-from muse.transforms.transforms import match_fov, reshape_x_to_slit_step
+from muse.transforms.transforms import match_fov, reshape_slit_step_to_x, reshape_x_to_slit_step
 
-__all__ = ["match_fov", "reshape_x_to_slit_step"]
+__all__ = ["match_fov", "reshape_slit_step_to_x", "reshape_x_to_slit_step"]
